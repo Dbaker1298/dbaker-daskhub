1334e29da682eedebbfe7bf931ab448cf6331d26
David Baker <dpbaker1298@gmail.com>
2022-12-01 15:05:54 -0500

    adding gitignore file with final deployment

--- deployment/12-deploy-daskhub.sh
+++ deployment/12-deploy-daskhub.sh
@@ -3,7 +3,7 @@
 ## Make absolutely sure you're ready to go and have edited all the files before you run this
 helm repo add dask https://helm.dask.org/
 helm repo update
-helm upgrade dhub dask/daskhub --namespace=daskhub --values=../helm/dh-config.yaml --values=../helm/dh-secrets.yaml --install --debug
+helm upgrade dhub dask/daskhub --namespace=daskhub --values=../helm/dh-config.yaml --values=../helm/dh-secrets.yaml --install --debug --version 2022.8.2
 
 
 ## After this is done, you still have work to do:
